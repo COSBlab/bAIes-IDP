393b7c171a490cdc40ca9f374bdc7beed07758a7
Vincent Schnapka <133697445+VSchnapka@users.noreply.github.com>
2025-06-25 10:35:17 +0200

    Delete tutorial/Plumed-Tutorials directory

--- tutorial/Plumed-Tutorials/tutorials/1-bAIes/1-preparation/step1-prepare_gmx.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-# takes a pdb model as input and makes gromacs files.
-name=idp
-
-echo 6 | gmx pdb2gmx -f ${1} -water none -o ${name}.gro -p ${name}.top -i ${name}.itp -ignh
-echo 0 | gmx trjconv -f ${name}.gro -s ${name}.gro -o ${name}.pdb
